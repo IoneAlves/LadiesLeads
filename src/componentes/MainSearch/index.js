684a741b58a01a56727a635cba1093a3fbbcb7ba
Ione Alves <ione.alves@ymail.com>
2022-11-12 23:41:13 -0300

    Iniciado lógica de retorno do resultado da pesquisa usando método fetch

--- src/componentes/MainSearch/index.js
+++ src/componentes/MainSearch/index.js
@@ -1,16 +1,67 @@
 import React from 'react';
 import './style.css'
 import Search from '../Search';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { ReactDOM } from 'react';
 
 
 function MainSearch (props) {
 
-    const [valorInicial , novoValor] = useState('teste')
-    
-    function searchActress () {
-        novoValor = React.getElementById('tvShow').value;        
+    const inputRef = useRef(null);
+    let [value, newValue] = useState('')
+
+    const api_key = 'b802fbd3e36b896f1ab90e5265d94830';
+
+    function getShow() {
+
+        //achar o nome da atriz
+        let showName = inputRef.current.value;
+
+            if (showName != '') {
+            const result = fetch(`https://api.genderize.io?name=${showName}`);
+            result
+                .then(reponse => reponse.json())
+                    .then(data => {
+                        if(data.gender == 'female'){
+                            console.log('É uma atriz');
+                        } else {
+                            console.log("Sorry, but this show isn't rulled by a girl.")
+                        }
+                    })
+                .catch(erro => console.log(erro))
+
+            let apiKey = `http://www.omdbapi.com/?t=${showName}&apikey=908b934c`;
+
+            let res = fetch(apiKey)
+            res
+                .then(resp => resp.json())
+                    .then(dataShow => console.log(dataShow.Actors[0]))
+                .catch(erro => console.log(erro))
+        }
     }
+
+
+    // if (name != '') {
+    //     const result = fetch(`https://api.genderize.io?name=${name}`);
+    //     result
+    //         .then(reponse => reponse.json())
+    //             .then(data => console.log(data))
+    //         .catch(erro => console.log(erro))
+    // }
+
+    
+    // async function searchName (value) {
+    //     try {
+    //         const result = await fetch(`https://api.genderize.io?name=${value}`);
+    //             result
+    //             .then(reponse => reponse.json())
+    //                 .then(data => console.log(data));
+    //     } catch (err) {
+    //         console.log(err);
+    //     }
+    // }
+
+    // searchName(value);        
 
     return (
         <section className='search'>
@@ -18,8 +69,8 @@ function MainSearch (props) {
                 <div className='search_content'>
                     <div className='search_content_form'>
                         <form className="form" action=''>
-                            <input className="form_input" type='text' id="tvShow" placeholder='Type the TV Show Name'></input>
-                            <button className="form_btn" type='button' onClick={searchActress}>Search</button>
+                            <input ref={inputRef} className="form_input" type='text' id="tvShow" placeholder='Type the TV Show Name'></input>
+                            <button className="form_btn" type='button' onClick={getShow}>Search</button>
                         </form>
                     </div>
                     <div className='search_content_return'>
@@ -27,7 +78,8 @@ function MainSearch (props) {
                             <img></img>
                         </div>
                         <div className='search_content_return_info'>
-                            <p className='return_info_text'>{valorInicial}</p>
+                            <p className='return_info_text'></p>
+                            <p itemID='testeRoot'></p>
                         </div>
                     </div>      
                 </div>
